040bd012c1721bdf8b8ebb7e5cd6a288d123fcc2
Felipe <felipedetoledomoraes@gmail.com>
2026-04-27 17:10:59 -0300

    new file:   Dominox/pom.xml 	new file:   Dominox/src/main/java/br/maua/dominox/Dominox.java 	deleted:    Hello.java

---
+++ Dominox/src/main/java/br/maua/dominox/Dominox.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.maua.dominox;
+
+/**
+ *
+ * @author felipe
+ */
+public class Dominox {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
