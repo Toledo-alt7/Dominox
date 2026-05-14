027a0aa15455a605b5379f9e9d10c7f19626f08c
Felipe <felipedetoledomoraes@gmail.com>
2026-05-14 17:15:43 -0300

    modified:   Dominox/src/main/java/br/maua/dominox/ActionHandler.java 	deleted:    Dominox/src/main/java/br/maua/dominox/Conexao.java 	modified:   Dominox/src/main/java/br/maua/dominox/DataBase.java 	modified:   Dominox/src/main/java/br/maua/dominox/LoginPage.java 	new file:   Dominox/src/main/java/br/maua/dominox/ValidateAccess.java 	deleted:    Dominox/src/main/java/br/maua/dominox/WelcomePage.java 	modified:   Dominox/src/main/java/br/maua/dominox/WindowPage.java 	modified:   README.md

--- Dominox/src/main/java/br/maua/dominox/WindowPage.java
+++ Dominox/src/main/java/br/maua/dominox/WindowPage.java
@@ -1,110 +1,20 @@
 package br.maua.dominox;
 
-import java.awt.*;
-import javax.swing.JButton;
+import java.awt.Font;
+
 import javax.swing.JFrame;
- 
+import javax.swing.JLabel;
+
 public class WindowPage {
-    final static boolean shouldFill = true;
-    final static boolean shouldWeightX = true;
-    final static boolean RIGHT_TO_LEFT = false;
- 
-    public static void addComponentsToPane(Container pane) {
-        if (RIGHT_TO_LEFT) {
-            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        }
- 
-    JButton button;
-    pane.setLayout(new GridBagLayout());
-    GridBagConstraints c = new GridBagConstraints();
-    if (shouldFill) {
-    //natural height, maximum width
-    c.fill = GridBagConstraints.HORIZONTAL;
-    }
- 
-    button = new JButton("Button 1");
-    
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 1;
-    c.gridy = 0;
-    c.ipady = 10;
-    c.insets = new Insets(0,150,20,150);  //top padding
-
-    pane.add(button, c);
- 
-    button = new JButton("Button 2");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 1;
-    c.gridy = 1;
-    c.ipady = 10;
-    c.insets = new Insets(0,150,20,150);  //top padding
-
-    pane.add(button, c);
- 
-    button = new JButton("Button 3");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 1;
-    c.insets = new Insets(0,150,20,150);  //top padding
-    c.ipady = 10;      //make this component tall
-
-    c.gridy = 2;
-    c.anchor = GridBagConstraints.PAGE_END;
-    pane.add(button, c);
- 
-    button = new JButton("4");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.ipady = 10;      //make this component tall
-    c.weightx = 0.0;
-    c.gridwidth = 1;
-    c.insets = new Insets(0,150,20,150);  //top padding
-
-    c.gridx = 1;
-    c.gridy = 3;
-    pane.add(button, c);
- 
-    button = new JButton("Chablau");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.ipady = 0;       //reset to default
-    c.weighty = 0.0;   //request any extra vertical space
-    c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-    c.insets = new Insets(0,150,20,150);  //top padding
-    c.gridx = 1;       //aligned with button 2
-    c.gridwidth = 0;   //2 columns wide
-    c.gridy = 4;       //third row
-    c.ipady = 10;
-    pane.add(button, c);
-
-    }
- 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    public static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Dominox");
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel("Hello!!");
+    public WindowPage(){
+        label.setBounds(0,0,100,50);
+        label.setFont(new Font(null, Font.BOLD,25));
+        frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
- 
-        //Display the window.
-        frame.pack();
+        frame.setSize(420,420);
+        frame.setLayout(null);
         frame.setVisible(true);
-        frame.setSize(500, 500);
-        frame.setMinimumSize(new Dimension(450, 400));
     }
- 
-    // public static void main(String[] args) {
-    //     //Schedule a job for the event-dispatching thread:
-    //     //creating and showing this application's GUI.
-    //     javax.swing.SwingUtilities.invokeLater(new Runnable() {
-    //         public void run() {
-    //             createAndShowGUI();
-    //         }
-    //     });
-    // }
 }
